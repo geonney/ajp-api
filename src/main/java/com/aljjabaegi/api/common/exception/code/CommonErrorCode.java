a7a677eb22e8cddcf609003ca48d77c45a48ac2f
GEONLEE <geonlee@neighbor21.co.kr>
2024-04-04 13:28:04 +0900

    [feature] ãadd ID_NOT_FOUND, change status

--- src/main/java/com/aljjabaegi/api/common/exception/code/CommonErrorCode.java
+++ src/main/java/com/aljjabaegi/api/common/exception/code/CommonErrorCode.java
@@ -4,7 +4,8 @@ package com.aljjabaegi.api.common.exception.code;
  * 공통 Exception enumeration
  *
  * @author GEONLEE
- * @since 2024-04-02
+ * @since 2024-04-02<br />
+ * 2024-04-04 GEONLEE - ID_NOT_FOUND 추가
  */
 public enum CommonErrorCode implements ErrorCode {
     SERVICE_ERROR("ER_SV_01", "요청한 서비스에 문제가 발생했습니다. 잠시 후에 다시 시도해 주세요."),
@@ -12,8 +13,9 @@ public enum CommonErrorCode implements ErrorCode {
     ENTITY_NOT_FOUND("ER_CT_02", "데이터가 존재하지 않습니다."),
     UNAUTHORIZED("ER_AT_01", "자격 증명에 실패하였습니다."),
     EXPIRED_TOKEN("ER_AT_02", "자격 증명에 실패하였습니다."),
-    WRONG_PASSWORD("ER_AT_03", "자격증명에 실패하였습니다."),
-    DUPLICATION_LOGIN("ER_AT_04", "자격증명에 실패하였습니다."),
+    ID_NOT_FOUND("ER_AT_03", "자격 증명에 실패하였습니다."),
+    WRONG_PASSWORD("ER_AT_04", "자격증명에 실패하였습니다."),
+    DUPLICATION_LOGIN("ER_AT_05", "자격증명에 실패하였습니다."),
     FORBIDDEN("ER_FD", "사용자 접근이 거부 되었습니다.");
 
     private final String status;
