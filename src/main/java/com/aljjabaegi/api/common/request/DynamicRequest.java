85bff1d1ae75f5fd514970579f355100cc0866f8
GEONLEE <geonlee@neighbor21.co.kr>
2024-04-12 11:21:21 +0900

    [feature] Add format_sql options

---
+++ src/main/java/com/aljjabaegi/api/common/request/DynamicRequest.java
@@ -0,0 +1,6 @@
+package com.aljjabaegi.api.common.request;/**
+ *
+ * @author GEONLEE
+ * @since 2024-04-12
+*/public record DynamicRequest() {
+}
---
+++ src/main/java/com/aljjabaegi/api/common/request/enumeration/SortDirections.java
@@ -0,0 +1,6 @@
+package com.aljjabaegi.api.common.request.enumeration;/**
+ *
+ * @author GEONLEE
+ * @since 2024-04-12
+*/public enum Sorters {
+}
