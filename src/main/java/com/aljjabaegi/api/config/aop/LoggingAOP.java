b818962ca705d72f32c547fe8f173c8cf91de2ec
GEONLEE <geonlee@neighbor21.co.kr>
2024-04-17 11:02:38 +0900

    [feature] Apply logging AOP

--- src/main/java/com/aljjabaegi/api/config/aop/LoggingAOP.java
+++ src/main/java/com/aljjabaegi/api/config/aop/LoggingAOP.java
@@ -1,5 +1,9 @@
-package kr.co.neighbor21.neighborApi.common.aop;
+package com.aljjabaegi.api.config.aop;
 
+import com.aljjabaegi.api.common.response.GridItemsResponse;
+import com.aljjabaegi.api.common.response.ItemResponse;
+import com.aljjabaegi.api.common.response.ItemsResponse;
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -9,104 +13,84 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.StringJoiner;
 
 /**
- * 모든 Post Request 의 Parameter 및 Response 로그파일 생성 AOP Class
- * 로그 파일 경로 : /logs/${project_name}_req_res.log
- * 이전 로그 폴더 경로 : /logs/03.request_response/
+ * HTTP method 별 AOP 로 동작하는 로깅 클래스
+ * resources/logback/logback-spring.xml 에서 설정하며, 파일 로깅한다.
  *
  * @author GEON LEE
- * @since 2023-11-16<br />
- * 2023-11-16 GEONLEE 최초 생성<br />
- * 2024-03-05 GEONLEE - List type parameter 도 로그 남기도록 printParameterFields 개선, parameter type 추가<br />
+ * @since 2024-04-16
  */
 @Aspect
 @Component
 public class LoggingAOP {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAOP.class);
 
-    @Before("@annotation(org.springframework.web.bind.annotation.PostMapping)")
-    public void logBefore(JoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
+    /**
+     * Around logging AOP
+     */
+    @Before("execution(public * com.aljjabaegi.api.domain.*.*Controller.*(..))")
+    public void loggingBefore(JoinPoint joinPoint) {
+        loggingRequest();
+        loggingParameter(joinPoint.getArgs());
+    }
+
+    /**
+     * Request 정보 로깅 method, URI
+     */
+    private void loggingRequest() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String requestUri = request.getRequestURI();
-        LOGGER.info("[Request URI] : {}", requestUri);
-        Arrays.stream(args)
-                .filter(Objects::nonNull)
-                .filter(arg -> !(arg instanceof SecurityContextHolderAwareRequestWrapper))
-                .forEach(this::printParameterFields);
+        LOGGER.info("[Request][{}] : {}", request.getMethod(), requestUri);
+    }
+
+    /**
+     * Request parameter logging
+     */
+    private void loggingParameter(Object objects) {
+        LOGGER.info("[Parameter] " + new Gson().toJson(objects));
     }
 
     /**
      * Logging Success Response
      *
      * @author GEON LEE
-     * @since 2023-11-16<br />
+     * @since 2024-04-17<br />
      */
-    @AfterReturning(pointcut = "@annotation(org.springframework.web.bind.annotation.PostMapping)", returning = "responseEntity")
-    public void logAfterReturning(JoinPoint joinPoint, ResponseEntity<?> responseEntity) {
-        LOGGER.info("◁◁◁◁◁◁[Response] Success.\n");
-    }
-
-    /**
-     * Logging Failure Response
-     *
-     * @author GEON LEE
-     * @since 2023-11-16<br />
-     */
-    @AfterThrowing(pointcut = "@annotation(org.springframework.web.bind.annotation.PostMapping)", throwing = "ex")
-    public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
-        LOGGER.info("◀◀◀◀◀[Response] Failure.\n" + "Exception : " + ex.getClass().getSimpleName() + "\nMessage : " + ex.getMessage() + "\n");
-    }
-
-    /**
-     * Request Parameter print Method
-     *
-     * @author GEON LEE
-     * @since 2023-11-16<br />
-     */
-    private void printParameterFields(Object obj) {
-        StringBuilder sb = new StringBuilder();
-        StringJoiner sj = new StringJoiner("\n");
-        if (obj instanceof List<?> listParameter) {
-            Class<?> parameter = listParameter.get(0).getClass();
-            sb.append("[Request Parameter] [").append(parameter.getSimpleName()).append("][List] ▷▷▷▷▷\n");
-            for (Object item : listParameter) {
-                Class<?> clazz = item.getClass();
-                Field[] fields = clazz.getDeclaredFields();
-                for (Field field : fields) {
-                    field.setAccessible(true);
-                    try {
-                        sj.add(field.getName() + " : " + field.get(item));
-                    } catch (IllegalAccessException e) {
-                        LOGGER.error("Parameter field logging failure.");
-                    }
-                }
+    @AfterReturning(pointcut = "execution(public * com.aljjabaegi.api.domain.*.*Controller.*(..))", returning = "responseEntity")
+    public void loggingAfterReturning(ResponseEntity<?> responseEntity) {
+        LOGGER.info("[Response][Success]");
+        Class<?> clazz = Objects.requireNonNull(responseEntity.getBody()).getClass();
+        Object body = responseEntity.getBody();
+        switch (clazz.getSimpleName()) {
+            case "ItemResponse" -> {
+                ItemResponse<?> itemResponse = (ItemResponse<?>) body;
+                LOGGER.info("[Body][ItemResponse][{}]{}\n", itemResponse.item().getClass().getSimpleName(), new Gson().toJson(itemResponse.item()));
             }
-        } else {
-            Class<?> clazz = obj.getClass();
-            Field[] fields = clazz.getDeclaredFields();
-            sb.append("[Request Parameter] [").append(clazz.getSimpleName()).append("] ▷▷▷▷▷\n");
-            for (Field field : fields) {
-                field.setAccessible(true);
-                try {
-                    sj.add(field.getName() + " : " + field.get(obj));
-                } catch (IllegalAccessException e) {
-                    LOGGER.error("Parameter field logging failure.");
-                }
+            case "ItemsResponse" -> {
+                ItemsResponse<?> itemsResponse = (ItemsResponse<?>) body;
+                LOGGER.info("[Body][ItemsResponse] size: {}\n", itemsResponse.size());
+            }
+            case "GridItemsResponse" -> {
+                GridItemsResponse<?> gridItemsResponse = (GridItemsResponse<?>) body;
+                Long totalSize = gridItemsResponse.totalSize();
+                int totalPageSize = gridItemsResponse.totalPageSize();
+                int size = gridItemsResponse.size();
+                LOGGER.info("[Body][GridItemsResponse] totalSize: {}, size: {}, totalPageSize: {}\n", totalSize, size, totalPageSize);
             }
         }
-        sb.append(sj);
-        LOGGER.info(sb.toString());
+    }
+
+    /**
+     * Logging Failure response
+     */
+    @AfterThrowing(pointcut = "execution(public * com.aljjabaegi.api.domain.*.*Controller.*(..))", throwing = "e")
+    public void loggingAfterThrowing(Throwable e) {
+        LOGGER.info("[Response][Failure] Exception: {}, message: {}\n", e.getClass().getSimpleName(), e.getMessage());
     }
 }
