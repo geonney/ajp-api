05ce1ce6bb45b9acb974caed72bbb525d255819d
GEONLEE <geonlee@neighbor21.co.kr>
2024-04-17 13:08:18 +0900

    [feature] Apply ë spring error controller implementation

--- src/main/java/com/aljjabaegi/api/config/security/springSecurity/CustomErrorController.java
+++ src/main/java/com/aljjabaegi/api/config/security/springSecurity/CustomErrorController.java
@@ -1,8 +1,23 @@
 package com.aljjabaegi.api.config.security.springSecurity;
 
+import com.aljjabaegi.api.common.exception.code.CommonErrorCode;
+import com.aljjabaegi.api.common.exception.custom.ServiceException;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
+ * Spring error controller implements
+ *
  * @author GEONLEE
  * @since 2024-04-17
  */
-public class ErrorController {
+@Controller
+public class CustomErrorController implements ErrorController {
+    @RequestMapping(value = "/error")
+    public ResponseEntity<ErrorResponse> error() {
+        throw new ServiceException(CommonErrorCode.INVALID_PARAMETER);
+    }
 }
