4e596aca7f34df1c2464d764b9b628c40fb2633b
GEONLEE <geonlee@neighbor21.co.kr>
2024-04-26 10:08:41 +0900

    [feature] 권한이 없는 익명 사용자 처리 추가, anonymousUser 조건 추가

--- src/main/java/com/aljjabaegi/api/common/jpa/auditorAware/SpringSecurityAuditorAware.java
+++ src/main/java/com/aljjabaegi/api/common/jpa/auditorAware/SpringSecurityAuditorAware.java
@@ -14,7 +14,8 @@ import java.util.Optional;
  * LastModifiedBy annotation 사용 시 MemberId 자동 바인딩
  *
  * @author GEONLEE
- * @since 2024-04-08
+ * @since 2024-04-08<br />
+ * 2024-04-26 GEONLEE 권한이 없는 익명 사용자 처리 추가, anonymousUser 조건 추가<br />
  */
 @Component
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
@@ -23,7 +24,7 @@ public class SpringSecurityAuditorAware implements AuditorAware<String> {
     @Nonnull
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
+        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
             return Optional.empty();
         }
         return Optional.of(((UserDetails) authentication.getPrincipal()).getUsername());
