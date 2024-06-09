5a487e3cb6e8ed4479a45e7813b3fb5af14cba0d
GEONLEE <geonlee@neighbor21.co.kr>
2024-06-10 08:42:29 +0900

    [feature] @SQLRestriction 추가

--- src/main/java/com/aljjabaegi/api/entity/Code.java
+++ src/main/java/com/aljjabaegi/api/entity/Code.java
@@ -6,14 +6,17 @@ import com.aljjabaegi.api.entity.key.CodeKey;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
 
 /**
  * @author GEONLEE
- * @since 2024-05-29
+ * @since 2024-05-29<br />
+ * 2024-06-10 @SQLRestriction 추가
  */
 @Getter
 @Setter
 @Entity(name = "code")
+@SQLRestriction("use_yn = 'Y'")
 public class Code extends BaseEntity {
 
     @EmbeddedId
